aea467ccbf73f25bcc4c08ba82188d20a83df4da
Pooja0793 <72892373+Pooja0793@users.noreply.github.com>
2021-01-29 12:33:09 +0530

    Create Stone.js

--- js/Stone.js
+++ js/Stone.js
@@ -1,8 +1,8 @@
 class  Stone{
-    constructor(x,y,width,height){
+    constructor(x,y,r){
     
-    this.body=Bodies.rectangle(x,y,width,height,{isStatic:false});
-    this.width=width;
+    this.body=Bodies.circle(x,y,r,{isStatic:false});
+    this.r=r;
     this.image=loadImage("images/stone.png")
     this.height=height;
     World.add(world, this.body);
@@ -17,10 +17,10 @@ class  Stone{
       push()
       
     translate(242,390);
-    rectMode(CENTER);
+    imageMode(CENTER);
     fill (255,0,255);
     rect(2,0,this.width,this.height);
-    image(this.image,0, 0,this.width, this.height)
+    image(this.image,0, 0,this.r)
     pop ();
     }
 }
